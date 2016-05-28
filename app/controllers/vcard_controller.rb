d92173476d43e81e8cf7b7cf607b951953b300eb
Misigno <efournier92@gmail.com>
2016-05-28 18:33:54 -0400

    Initial Commit

---
+++ app/controllers/vcard_controller.rb
@@ -0,0 +1,2 @@
+class VcardController < ApplicationController
+end
---
+++ spec/helpers/vcard_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the VcardHelper. For example:
+#
+# describe VcardHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe VcardHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
